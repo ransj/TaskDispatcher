9a3f3cfa95988a8ddcb622b755de5d3bd9558005
ransj <ranshengju@gmail.com>
2017-07-06 06:17:49 +0800

    fix runtime class cast exception

--- src/main/java/os/ransj/task/SimpleJob.java
+++ src/main/java/os/ransj/task/SimpleJob.java
@@ -4,9 +4,11 @@ package os.ransj.task;
  * Created by ranshengju on 04/07/2017.
  */
 
-public abstract class SimpleJob extends Job<Void, Void> {
+public abstract class SimpleJob extends Job<java.lang.Void, java.lang.Void> {
+
     @Override
-    public Void execute(Void... params) {
+    public Void execute(Void[] params) {
+        execute();
         return null;
     }
 
--- src/main/java/os/ransj/task/VoidJob.java
+++ src/main/java/os/ransj/task/VoidJob.java
@@ -4,13 +4,14 @@ package os.ransj.task;
  * Created by ranshengju on 25/06/2017.
  */
 
-public abstract class VoidJob<P> extends Job<P, Void> {
+public abstract class VoidJob<P> extends Job<P, java.lang.Void> {
 
     @Override
-    public Void execute(P... params) {
+    public java.lang.Void execute(P... params) {
         executeVoid(params);
         return null;
     }
 
     public abstract void executeVoid(P... params);
+
 }
